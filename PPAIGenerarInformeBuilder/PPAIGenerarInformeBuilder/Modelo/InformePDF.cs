979576d238332cad3daaa53a50a2f317b5396cf3
marchetti554 <marchetti554@gmail.com>
2019-11-13 16:37:53 -0300

    fixed impl

---
+++ PPAIGenerarInformeBuilder/PPAIGenerarInformeBuilder/Modelo/InformePDF.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PPAIGenerarInformeBuilder.Modelo
+{
+    public class InformePDF
+    {
+        string titulo;
+        string nombreUsuario;
+        DateTime fechaDesde;
+        DateTime fechaHasta;
+        DateTime fechaHoraGeneracion;
+        Chart grafico;
+
+        public void setEncabezado(DateTime fechaDesde, DateTime fechaHasta, string titulo)
+        {
+            this.fechaDesde = fechaDesde;
+            this.fechaHasta = fechaHasta;
+            this.titulo = titulo;
+        }
+        public void setCuerpo(Chart chart)
+        {
+            this.grafico = chart;
+        }
+        public void setPie(DateTime fechaHoraGeneracion, string nombreUsuario)
+        {
+            this.fechaHoraGeneracion = fechaHoraGeneracion;
+            this.nombreUsuario = nombreUsuario;
+        }
+    }
+}
